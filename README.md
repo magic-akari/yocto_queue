eb3acb63ff5c197fa18881ba541910975d547aa5
magic-akari <akari.ccino@gmail.com>
2021-05-03 20:37:46 +0800

    chore: 🚀 first draft

---
+++ README.md
@@ -0,0 +1,52 @@
+Same as [yocto-queue](https://www.npmjs.com/package/yocto-queue) but for Deno.
+
+## Usage
+
+```typescript
+import { Queue } from "https://github.com/magic-akari/yocto_queue/queue.ts";
+
+const queue = new Queue();
+
+queue.enqueue("🦄");
+queue.enqueue("🌈");
+
+console.log(queue.size);
+//=> 2
+
+console.log(...queue);
+//=> '🦄 🌈'
+
+console.log(queue.dequeue());
+//=> '🦄'
+
+console.log(queue.dequeue());
+//=> '🌈'
+```
+
+## API
+
+### `queue = new Queue()`
+
+The instance is an [`Iterable`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Iteration_protocols), which means you can iterate over the queue front to back with a “for…of” loop, or use spreading to convert the queue to an array. Don't do this unless you really need to though, since it's slow.
+
+#### `.enqueue(value)`
+
+Add a value to the queue.
+
+#### `.enqueue(...values)`
+
+Add multiply values to the queue.
+
+#### `.dequeue()`
+
+Remove the next value in the queue.
+
+Returns the removed value or `undefined` if the queue is empty.
+
+#### `.clear()`
+
+Clear the queue.
+
+#### `.size`
+
+The size of the queue.
---
+++ queue_test.ts
@@ -0,0 +1,67 @@
+import { assertEquals } from "https://deno.land/std@0.95.0/testing/asserts.ts";
+import { Queue } from "./queue.ts";
+
+Deno.test(".enqueue()", () => {
+  const queue = new Queue();
+  queue.enqueue("🦄");
+  assertEquals(queue.dequeue(), "🦄");
+  queue.enqueue("🌈");
+  queue.enqueue("❤️");
+  assertEquals(queue.dequeue(), "🌈");
+  assertEquals(queue.dequeue(), "❤️");
+});
+
+Deno.test(".enqueue(...)", () => {
+  const queue = new Queue();
+  queue.enqueue("🦄");
+  assertEquals(queue.dequeue(), "🦄");
+  queue.enqueue("🌈", "❤️");
+  assertEquals(queue.dequeue(), "🌈");
+  assertEquals(queue.dequeue(), "❤️");
+});
+
+Deno.test(".dequeue()", () => {
+  const queue = new Queue();
+  assertEquals(queue.dequeue(), undefined);
+  assertEquals(queue.dequeue(), undefined);
+  queue.enqueue("🦄");
+  assertEquals(queue.dequeue(), "🦄");
+  assertEquals(queue.dequeue(), undefined);
+});
+
+Deno.test(".clear()", () => {
+  const queue = new Queue();
+  queue.clear();
+  queue.enqueue(1);
+  queue.clear();
+  assertEquals(queue.size, 0);
+  queue.enqueue(1);
+  queue.enqueue(2);
+  queue.enqueue(3);
+  queue.clear();
+  assertEquals(queue.size, 0);
+});
+
+Deno.test(".size", () => {
+  const queue = new Queue();
+  assertEquals(queue.size, 0);
+  queue.clear();
+  assertEquals(queue.size, 0);
+  queue.enqueue("🦄");
+  assertEquals(queue.size, 1);
+  queue.enqueue("🦄");
+  assertEquals(queue.size, 2);
+  queue.dequeue();
+  assertEquals(queue.size, 1);
+  queue.dequeue();
+  assertEquals(queue.size, 0);
+  queue.dequeue();
+  assertEquals(queue.size, 0);
+});
+
+Deno.test("iterable", () => {
+  const queue = new Queue();
+  queue.enqueue("🦄");
+  queue.enqueue("🌈");
+  assertEquals([...queue], ["🦄", "🌈"]);
+});
